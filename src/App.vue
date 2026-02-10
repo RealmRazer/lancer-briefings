f3b697f9c34b1309011f263fb5c6c898a96c27fd
RealmRazer <shadowsolider@gmail.com>
2026-02-10 12:17:12 -0500

    New base line and "rebase" with main.

--- src/App.vue
+++ src/App.vue
@@ -1,259 +1,165 @@
 <template>
-  <Header :header="this.header" />
-  <div class="content-container">
-    <section class="section-container" id="missions" style="width:435px; height:714px;">
-      <div class="section-header clipped-medium-backward">
-        <img src="/icons/mission-icon.svg" />
-        <h1>Mission Log</h1>
-      </div>
-      <div class="section-content-container">
-        <h3>Current Assignment</h3>
-        <Markdown :source="current_md" class="markdown" />
-        <h3>Mission List</h3>
-        <div class="mission-list-container">
-          <Mission
-            v-for="item in this.missions"
-            :key="item.slug"
-            :mission="item"
-            :selected="this.mission_slug"
-            @click="selectMission(item)"
-          />
-        </div>
-      </div>
-    </section>
-    <section class="section-container" id="events" style="width:435px; height:714px;">
-      <div class="section-header clipped-medium-backward">
-        <img src="/icons/events-icon.svg" />
-        <h1>Events Log</h1>
-      </div>
-      <div class="section-content-container">
-        <Markdown :source="events" class="markdown" />
-      </div>
-    </section>
-    <section class="section-container" id="pilots" style="width:894px; height:714px;">
-      <div style="height:52px; overflow:hidden;">
-        <div class="section-header clipped-medium-backward-pilot">
-          <img src="/icons/pilot-icon.svg" />
-          <h1>Pilot Roster</h1>
-        </div>
-        <div class="rhombus-back">&nbsp;</div>
-      </div>
-      <div class="section-content-container">
-        <div class="pilot-list-container">
-          <Pilot v-for="item in this.pilots" :key="item.slug" :pilot="item" />
-        </div>
-      </div>
-    </section>
-  </div>
-  <svg
-    style="visibility: hidden; position: absolute;"
-    width="0"
-    height="0"
-    xmlns="http://www.w3.org/2000/svg"
-    version="1.1"
-  >
-    <defs>
-      <filter id="round">
-        <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
-        <feColorMatrix
-          in="blur"
-          mode="matrix"
-          values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -5"
-          result="goo"
-        />
-        <feComposite in="SourceGraphic" in2="goo" operator="atop" />
-      </filter>
-    </defs>
-  </svg>
-  <audio autoplay>
-    <source src="/startup.ogg" type="audio/ogg" />
-  </audio>
-  <Footer/>
+	<div class="page-wrapper">
+		<Header :planet-path="planetPath" :class="{ animate: animate }" :header="header" />
+		<Sidebar :animate="animate" :class="{ animate: animate }" />
+	</div>
+	<div id="router-view-container">
+		<router-view :animate="animate" :initial-slug="initialSlug" :missions="missions" :events="events"
+			:pilots="pilots" :clocks="clocks" :reserves="reserves" />
+	</div>
+	<svg style="visibility: hidden; position: absolute" width="0" height="0" xmlns="http://www.w3.org/2000/svg"
+		version="1.1">
+		<defs>
+			<filter id="round">
+				<feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
+				<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -5"
+					result="goo" />
+				<feComposite in="SourceGraphic" in2="goo" operator="atop" />
+			</filter>
+		</defs>
+	</svg>
+	<audio autoplay>
+		<source src="/startup.ogg" type="audio/ogg" />
+	</audio>
 </template>
 
 <script>
-import Header from './components/layout/Header.vue';
-import Footer from './components/layout/Footer.vue';
-import Mission from './components/Mission.vue';
-import Pilot from './components/Pilot.vue';
-import Markdown from 'vue3-markdown-it';
+import Header from "./components/layout/Header.vue";
+import Sidebar from "./components/layout/Sidebar.vue";
+import Config from "@/assets/info/general-config.json";
 
 export default {
-  components: {
-    Header,
-    Footer,
-    Mission,
-    Pilot,
-    Markdown
-  },
+	components: {
+		Header,
+		Sidebar,
+	},
 
-  data() {
-    return {
-      "mission_slug": "006",
-      "current_md": "",
-      "events": "",
-      "missions": [
-      {
-          "slug": "006",
-          "name": "Sic Semper",
-          "status": "start"
-        },
-        {
-          "slug": "005",
-          "name": "Grassroots",
-          "status": "success"
-        },
-        {
-          "slug": "004",
-          "name": "Burn Down the House",
-          "status": "partial-success"
-        },
-        {
-          "slug": "003",
-          "name": "Pulling Threads",
-          "status": "success"
-        },
-        {
-          "slug": "002",
-          "name": "Steelworks",
-          "status": "success"
-        },
-        {
-          "slug": "001",
-          "name": "The Long Night",
-          "status": "success"
-        },
-      ],
-      "pilots": [
-        {
-          "callsign": "BOB",
-          "alias": "Brie O'Brien",
-          "code": "b91cbe56-1948-9680-0901d615cf35118d//NDL-C-DEEP-STATION",
-          "corpro": "IPS-N",
-          "frame": "VLAD",
-          "mech": "Spikey Boi"
-        },
-        {
-          "callsign": "Cowboy",
-          "alias": "Andy Altman",
-          "code": "000fc99e-a855-4720-c6aae10011771b7a//NDL-C-DEEP-STATION",
-          "corpro": "IPS-N",
-          "frame": "Raleigh",
-          "mech": "Onyx 2.0"
-        },
-        {
-          "callsign": "Jayhawk",
-          "alias": "Cvetanka Solverson",
-          "code": "dee3549a-3f1c-9981-0396c277463b75dd//NDL-C-MECCOV-PROPER",
-          "corpro": "SSC",
-          "frame": "Mourning Cloak",
-          "mech": "99 Problems"
-        },
-        {
-          "callsign": "Righteous",
-          "alias": "Tristan Karkov",
-          "code": "def185ee-5b59-f3ed-ef659ca5caded34a//NDL-C-UNKNOWN",
-          "corpro": "Horus",
-          "frame": "Goblin",
-          "mech": "Gremlin"
-        },
-        {
-          "callsign": "Rusty Shell",
-          "alias": 'Damir "Petrov" Radonić',
-          "code": "23a5b917-186f-5fe9-1fe5f8dd10f772d0//NDL-C-MECCOV-PROPER",
-          "corpro": "IPS-N",
-          "frame": "Tortuga",
-          "mech": "Uranium Core"
-        },
-        {
-          "callsign": "Spider",
-          "alias": 'Viktor Murakami',
-          "code": "95974952-fbf5-a982-14f520982199a1ef//NDL-C-MECCOV-PROPER",
-          "corpro": "SSC",
-          "frame": "Swallowtail",
-          "mech": "Recluse"
-        },
-        {
-          "callsign": "Sterling",
-          "alias": 'Archer Aster Silver',
-          "code": "f9aaf53b-a22d-287f-39a5d7fff1976c7d//NDL-C-MECCOV-PROPER",
-          "corpro": "SSC",
-          "frame": "Monarch",
-          "mech": "Woodhouse"
-        },
-      ],
-      "header": {
-        "planet": "Meccov::Srada",
-        "year": "50XXu",
-        "system": "Gemini Basin",
-        "gate": "xxx",
-        "ring": "Alterna-Line",
-        "headerTitle": "JMR Agency",
-        "headerSubtitle": "Intelligence Services",
-        "subheaderTitle": "Intelligence",
-        "subheaderSubtitle": "Security",
-      },
-      "options":{
-        "eventsMarkdownPerMission": true
-      }
-    }
-  },
+	data() {
+		return {
+			animate: Config.animate,
+			initialSlug: Config.initialSlug,
+			planetPath: Config.planetPath,
+			header: Config.header,
+			pilotSpecialInfo: Config.pilotSpecialInfo,
+			clocks: [],
+			events: [],
+			missions: [],
+			pilots: [],
+			reserves: [],
+			bonds: [],
+		};
+	},
+	created() {
+		this.setTitleFavicon(Config.defaultTitle + " MISSION BRIEFING", Config.icon);
+		this.importMissions(import.meta.glob("@/assets/missions/*.md", { query: '?raw', import: 'default' }));
+		this.importEvents(import.meta.glob("@/assets/events/*.md", { query: '?raw', import: 'default' }));
+		this.importClocks(import.meta.glob("@/assets/clocks/*.json"));
+		this.importReserves(import.meta.glob("@/assets/reserves/*.json"));
+		this.importPilots(import.meta.glob("@/assets/pilots/*.json"));
+	},
+	mounted() {
+		this.$router.push("/status");
+	},
+	methods: {
+		setTitleFavicon(title, favicon) {
+			document.title = title;
+			let headEl = document.querySelector('head');
+			let faviconEl = document.createElement('link');
+			faviconEl.setAttribute('rel', 'shortcut icon');
+			faviconEl.setAttribute('href', favicon);
+			headEl.appendChild(faviconEl);
+		},
+		async importMissions(files) {
+			let filePromises = Object.keys(files).map(path => files[path]());
+			let fileContents = await Promise.all(filePromises);
+			fileContents.forEach(content => {
+				let mission = {};
+				mission["slug"] = content.split("\n")[0];
+				mission["name"] = content.split("\n")[1];
+				mission["status"] = content.split("\n")[2];
+				mission["content"] = content.split("\n").splice(3).join("\n");
+				this.missions = [...this.missions, mission];
+			});
+			this.missions = this.missions.sort(function (a, b) {
+				return b["slug"] - a["slug"];
+			})
+		},
+		async importEvents(files) {
+			let filePromises = Object.keys(files).map(path => files[path]());
+			let fileContents = await Promise.all(filePromises);
+			fileContents.forEach(content => {
+				let event = {};
+				event["title"] = content.split("\n")[0];
+				event["location"] = content.split("\n")[1];
+				event["time"] = content.split("\n")[2];
+				event["thumbnail"] = content.split("\n")[3];
+				event["content"] = content.split("\n").splice(4).join("\n");
+				this.events = [...this.events, event];
+			});
+			this.events = this.events.reverse();
+		},
+		async importClocks(files) {
+			let filePromises = Object.keys(files).map(path => files[path]());
+			let fileContents = await Promise.all(filePromises);
+			fileContents.forEach(content => {
+				this.clocks = JSON.parse(JSON.stringify(content)).default;
+			});
+		},
+		async importReserves(files) {
+			let filePromises = Object.keys(files).map(path => files[path]());
+			let fileContents = await Promise.all(filePromises);
+			fileContents.forEach(content => {
+				this.reserves = JSON.parse(JSON.stringify(content)).default;
+			});
+		},
+		async importPilots(files) {
+			let filePromises = Object.keys(files).map(path => files[path]());
+			let fileContents = await Promise.all(filePromises);
+			fileContents.forEach(content => {
+				let pilotFromJson = JSON.parse(JSON.stringify(content));
+				// In case the pilot was added from a copy on compcon via sharecode, remove the "reference mark" symbol
+				pilotFromJson.name = pilotFromJson.name.replace("※", "");
+				pilotFromJson.callsign = pilotFromJson.callsign.replace("※", "");
+				let pilotFromVue = this.pilotSpecialInfo[pilotFromJson.callsign.toUpperCase()];
+				let pilot = {
+					...pilotFromJson,
+					...pilotFromVue,
+				};
+				this.pilots = [...this.pilots, pilot];
+				pilot.clocks.forEach(content => {
+					let clock = {};
+					clock["type"] = `Pilot Project // ${pilot.callsign}`;
+					clock["result"] = "";
+					clock["name"] = content.title;
+					clock["description"] = content.description;
+					clock["value"] = content.progress;
+					clock["max"] = content.segments;
+					clock["color"] = "#3CB043";
+					this.clocks = [...this.clocks, clock];
+				});
 
-  created() {
-    this.loadMissionMarkdown()
-    this.loadEventsMarkdown()
-  },
-
-  computed: {
-
-  },
-
-  methods: {
-    selectMission(mission) {
-      this.mission_slug = mission.slug;
-      this.loadMissionMarkdown()
-      if(this.options.eventsMarkdownPerMission){
-        this.loadEventsMarkdown();
-      }
-    },
-    loadMissionMarkdown() {
-      let self = this;
-      let md = `/missions/${self.mission_slug}.md`
-      var client = new XMLHttpRequest();
-      client.open('GET', md);
-      client.onreadystatechange = function () {
-        self.current_md = client.responseText;
-      }
-      client.send();
-    },
-    loadEventsMarkdown() {
-      let self = this;
-      let md = "";
-
-      if(self.options.eventsMarkdownPerMission){
-        md = `/events/${self.mission_slug}.md`
-      }
-      else {
-        md = "/events.md"
-      }
-
-      var client = new XMLHttpRequest();
-      client.open('GET', md);
-      client.onreadystatechange = function () {
-        self.events = client.responseText;
-      }
-      client.send();
-    }
-  }
-
-}
+				pilot.reserves.forEach(content => {
+					let reserve = {};
+					reserve["type"] = content.type;
+					reserve["name"] = content.name;
+					reserve["description"] = content.description;
+					reserve["label"] = content.label;
+					reserve["cost"] = content.cost;
+					reserve["notes"] = content.notes;
+					reserve["callsign"] = pilot.callsign.toUpperCase();
+					this.reserves = [...this.reserves, reserve];
+				});
+			});
+		},
+	},
+};
 </script>
 
-
-<style lang="scss">
+<style>
 #app {
-  width: 1902px;
-  height: 910px;
-  overflow: hidden;
+	min-height: 100vh;
+	overflow: hidden !important;
+	/* border-right: 1px solid #ff0;
+	border-bottom: 1px solid #ff0; */
 }
 </style>
